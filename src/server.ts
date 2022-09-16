53efe32b37c887d268b1ccbe985e034646ac7ba3
Tiago Rodrigues <tiagoasrodrigues@protonmail.com>
2022-09-16 09:11:09 -0300

    :wrench: Setup express

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ app.get('/ads', (req, res) => {
     { id: 1, name: "Anúncio 1" },
     { id: 2, name: "Anúncio 2" },
     { id: 3, name: "Anúncio 3" },
+    { id: 4, name: "Anúncio 4" },
   ])
 })
 
